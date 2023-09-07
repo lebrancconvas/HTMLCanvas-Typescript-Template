63ef6a1464e6eb390b26c8e6020f17a28a54cb31
lebrancconvas <lebrancconvas@gmail.com>
2023-09-07 19:20:56 +0700

    ✨ feat: update game loop

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,15 @@ canvas.height = config.height;
 canvas.style.backgroundColor = config.backgroundColor;
 
 // Code.
+
+// Init.
+
+
+// Game Loop.
+function animate() {
+  window.requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // Animation Code.
+};
+
+animate();
